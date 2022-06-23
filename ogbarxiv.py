bd4972401a219bd0d02eac7dc5bb2e68edb8fcfe
yimengmin <yimengmin@outlook.com>
2022-06-23 00:25:02 -0400

    update model

--- ogbarxiv.py
+++ ogbarxiv.py
@@ -19,15 +19,18 @@ import torch.nn as nn
 from torch_geometric.datasets import WikiCS
 from torch_geometric.utils import to_scipy_sparse_matrix
 import torch_geometric.transforms as T
-path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'WikiCS')
 from torch_geometric.utils import to_scipy_sparse_matrix
-from utils import normalize_adjacency_matrix,normalizemx
-from utils import normalize_adjacency_matrix,accuracy,scattering1st,sparse_mx_to_torch_sparse_tensor
+from utils import sparse_mx_to_torch_sparse_tensor
 from layers import GC_withres
 import torch.optim as optim
 import numpy as np
 from models import SCT_GAT_ogbarxiv
 
+
+
+
+from ogb.nodeproppred import Evaluator #use to evalatute the accuracy
+evaluator = Evaluator("ogbn-arxiv")
 ### use gcn
 #from torch_geometric.nn import GCNConv, ChebConv  # noqa
 from layers import GC_withres,GC
@@ -39,15 +42,15 @@ parser.add_argument('--fastmode', action='store_true', default=False,
 parser.add_argument('--seed', type=int, default=42, help='Random seed.')
 parser.add_argument('--epochs', type=int, default=200,
                     help='Number of epochs to train.')
-parser.add_argument('--lr', type=float, default=0.005,
+parser.add_argument('--lr', type=float, default=0.001,
                     help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=5e-5,
                     help='Weight decay (L2 loss on parameters).')
 parser.add_argument('--l1', type=float, default=0.0,
                     help='Weight decay (L1 loss on parameters).')
-parser.add_argument('--hid', type=int, default=60,
+parser.add_argument('--hid', type=int, default=64,
                     help='Number of hidden units.')
-parser.add_argument('--nheads', type=int, default=10,
+parser.add_argument('--nheads', type=int, default=1,
                     help='Number of heads in attention mechism.')
 parser.add_argument('--data_spilt', type=int, default=0)
 parser.add_argument('--patience', type=int, default=200)
@@ -60,8 +63,6 @@ parser.add_argument('--smoo', type=float, default=0.1,
                     help='Smooth for Res layer')
 parser.add_argument('--use_gdc', action='store_true',
                     help='Use GDC preprocessing.')
-parser.add_argument('--nlayers', type=int, default=2,
-                    help='num of layers')
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
@@ -80,8 +81,6 @@ dataset = PygNodePropPredDataset(name="ogbn-arxiv")
 split_idx = dataset.get_idx_split()
 
 
-train_idx = split_idx['train'].to(device)
-print(train_idx)
 data = dataset[0]
 device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 x = data.x.to(device)
@@ -96,27 +95,41 @@ adj = adj+ adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
 features = data.x
 features = torch.FloatTensor(np.array(features))
 features = features.to(device)
-#model = GCN(nfeat=features.shape[1],nhid=64,nclass=10,nlayers=args.nlayers,dropout=args.dropout)
-
-#labels = data.y
-#labels = torch.LongTensor(np.array(labels))
-#labels = labels.to(device)
-#print(features.shape)
-#print("========================")
-#print('Loading')
-#adj_sct1 = scattering1st(adj_p,1)
-#adj_sct2 = scattering1st(adj_p,2)
-#adj_sct4 = scattering1st(adj_p,3)
-#adj_p = sparse_mx_to_torch_sparse_tensor(adj_p)
-#A_tilde = sparse_mx_to_torch_sparse_tensor(A_tilde)
-from diffusion import GCN_diffusion,scattering_diffusion
 adj = sparse_mx_to_torch_sparse_tensor(adj).cuda()
-gcn_diffusion_list = GCN_diffusion(adj,3,features)
-print(gcn_diffusion_list[1].size())
-h_sct1,h_sct2,h_sct3 = scattering_diffusion(adj,features)
-print(h_sct1.size())
-model = SCT_GAT_ogbarxiv(features.shape[1],args.hid,10,dropout=args.dropout,nheads=args.nheads,smoo=args.smoo)
+#from diffusion import GCN_diffusion,scattering_diffusion
+#gcn_diffusion_list = GCN_diffusion(adj,3,features)
+#print(gcn_diffusion_list[1].size())
+#h_sct1,h_sct2,h_sct3 = scattering_diffusion(adj,features)
+#print(h_sct1.size())
+model = SCT_GAT_ogbarxiv(features.shape[1],args.hid,dataset.num_classes,dropout=args.dropout,nheads=args.nheads,smoo=args.smoo)
 model = model.cuda()
-out_feature = model(features,adj) 
-print('=====')
-print(out_feature.size())
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+#pred = out_feature[train_idx]
+
+
+### y true
+y_true = data.y.to(device)
+#y_true = y_true[split_idx['train']]
+
+
+
+def train(epoch):
+    global valid_error
+    t = time.time()
+    model.train()
+    optimizer.zero_grad()
+    out_feature = model(features,adj) 
+    y_pred = out_feature.argmax(dim=-1, keepdim=True)
+#    print(y_pred.size())
+#    loss_train = F.nll_loss(y_pred[split_idx['train']], y_true.squeeze(1)[split_idx['train']])
+    print('---')
+    loss_train = F.nll_loss(out_feature[split_idx['train']], y_true.squeeze(1)[split_idx['train']])
+#    acc_train = accuracy(out_feature[split_idx['train']], y_true[split_idx['train']])
+    acc_train = evaluator.eval({'y_true': y_true[split_idx['train']],'y_pred': y_pred[split_idx['train']],})['acc']
+    print(acc_train)
+    loss_train.backward()
+    optimizer.step()
+
+
+for i in range(args.epochs):
+    train(i)
